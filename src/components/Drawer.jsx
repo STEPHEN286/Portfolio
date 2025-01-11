73aae72a63d09c92d71299db82664796378b4231
STEPHEN286 <kwofie286@gmail.com>
2025-01-11 12:28:21 +0000

    update on drawer

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -3,6 +3,8 @@
 
 import {  Drawer } from "flowbite-react";
 import myImage from "../asset/images/my-img.jpg"
+import myQr from "../asset/images/myqrcode.png"
+import myGraphic from "../asset/images/qrBio.png"
 
 export function ImageDrawer({isOpen, setIsOpen}) {
 
@@ -16,8 +18,18 @@ export function ImageDrawer({isOpen, setIsOpen}) {
         <Drawer.Header />
         <Drawer.Items>
          
-         <div>
+         <div className="flex flex-col items-center justify-center h-screen">
           <img src={myImage} alt="my-image" />
+         
+         <div className="flex flex-col items-center justify-center h-screen">
+          <p>On Linkedin</p>
+         <img src={myQr} alt="my-image" />
+         </div>
+         <div className="flex flex-col items-center justify-center h-screen">
+          <p>Graphic Work</p>
+         <img src={myGraphic} alt="my-image" />
+         </div>
+
          </div>
          
         </Drawer.Items>
